1e7fd4a8e4cd94f0e51092f22b65213f1d2df3f7
kevin <kevin@kevin.com>
2017-07-01 22:09:21 +0800

    draw triangle

--- lua/device.lua
+++ lua/device.lua
@@ -88,9 +88,136 @@ end
 -----------------------------------------
 
 function Device:drawTriangle(pa, pb, pc, color)
-	self:drawLine(pa, pb, color)
-	self:drawLine(pa, pc, color)
-	self:drawLine(pc, pb, color)
+	local x1 = pa.x
+	local y1 = pa.y
+	local x2 = pb.x
+	local y2 = pb.y
+	local x3 = pc.x
+	local y3 = pc.y
+
+	if y1 == y2 then
+		if y3 <= y1 then -- 平底
+			self:drawTriangle_bottom(pc, pa, pb, color)
+		else -- 平顶
+			self:drawTriangle_top(pa, pb, pc, color)
+		end
+	elseif y1 == y3 then
+		if y2 <= y1 then -- 平地
+			self:drawTriangle_bottom(pb, pa, pc, color)
+		else -- 平顶
+			self:drawTriangle_top(pa, pc, pb, color)
+		end
+	elseif y2 == y3 then
+		if y1 <= y2 then -- 平底
+			self:drawTriangle_bottom(pa, pb, pc, color)
+		else -- 平顶
+			self:drawTriangle_top(pb, pc, pa, color)
+		end
+	else
+		local xtop, ytop, xmid, ymid, xbot, ybot
+		
+		if y1 < y2 and y2 < y3 then -- y1 y2 y3
+			xtop = x1
+			ytop = y1
+			xmid = x2
+			ymid = y3
+			xbot = x2
+			ybot = y2
+		elseif y1 < y3 and y3 < y2 then -- y1 y3 y2
+			xtop = x1
+			ytop = y1
+			xmid = x3
+			ymid = y3
+			xbot = x2
+			ybot = y2
+		elseif y2 < y1 and y1 < y3 then -- y2 y1 y3
+			xtop = x2
+			ytop = y2
+			xmid = x1
+			ymid = y1
+			xbot = x3
+			ybot = y3
+		elseif y2 < y3 and y3 < y1 then -- y2 y3 y1
+			xtop = x2
+			ytop = y2
+			xmid = x3
+			ymid = y3
+			xbot = x1
+			ybot = y1
+		elseif y3 < y1 and y1 < y2 then -- y3 y1 y2
+			xtop = x3
+			ytop = y3
+			xmid = x1
+			ymid = y1
+			xbot = x2
+			ybot = y2
+		elseif y3 < y2 and y2 < y1 then -- y3 y2 y1
+			xtop = x3
+			ytop = y3
+			xmid = x2
+			ymid = y2
+			xbot = x1
+			ybot = y1
+		end
+
+		local xl = (ymid - ytop) * (xtop - xtop) / (ybot - ytop) + xtop + 0.5
+
+		if x1 < xmid then
+			local v1 = Vector4.new(xtop, ytop, 0, 1)
+			local v2 = Vector4.new(xl, ymid, 0, 1)
+			local v3 = Vector4.new(xmid, ymid, 0, 1)
+			self:drawTriangle_bottom(v1, v2, v3, color)
+			local v4 = Vector4.new(xl, ymid, 0, 1)
+			local v5 = Vector4.new(xmid, ymid, 0, 1)
+			local v6 = Vector4.new(xbot, ybot, 0, 1)
+			self:drawTriangle_top(v4, v5, v6, color)
+		else
+			local v1 = Vector4.new(xtop, ytop, 0, 1)
+			local v2 = Vector4.new(xmid, ymid, 0, 1)
+			local v3 = Vector4.new(xl, ymid, 0, 1)
+			self:drawTriangle_bottom(v1, v2, v3, color)
+			local v4 = Vector4.new(xmid, ymid, 0, 1)
+			local v5 = Vector4.new(xl, ymid, 0, 1)
+			local v6 = Vector4.new(xbot, ybot, 0, 1)
+			self:drawTriangle_top(v4, v5, v6, color)
+		end
+	end
+end
+
+function Device:drawTriangle_top(pa, pb, pc, color)
+	local x1 = pa.x
+	local y1 = pa.y
+	local x2 = pb.x
+	local y2 = pb.y
+	local x3 = pc.x
+	local y3 = pc.y
+
+	for y = y1, y3 do
+		local xs = (y - y1) * (x3 - x1) / (y3 - y1) + x1 + 0.5
+		local xe = (y - y2) * (x3 - x2) / (y3 - y2) + x2 + 0.5
+
+		local v1 = Vector4.new(xs, y, 0, 1)
+		local v2 = Vector4.new(xe, y, 0, 1)
+		self:drawLine(v1, v2, color)
+	end
+end
+
+function Device:drawTriangle_bottom(pa, pb, pc, color)
+	local x1 = pa.x
+	local y1 = pa.y
+	local x2 = pb.x
+	local y2 = pb.y
+	local x3 = pc.x
+	local y3 = pc.y
+
+	for y = y1, y2 do
+		local xs = (y - y1) * (x2 - x1) / (y2 - y1) + x1 + 0.5
+		local xe = (y - y1) * (x3 - x1) / (y3 - y1) + x1 + 0.5
+
+		local v1 = Vector4.new(xs, y, 0, 1)
+		local v2 = Vector4.new(xe, y, 0, 1)
+		self:drawLine(v1, v2, color)
+	end
 end
 
 -----------------------------------------
